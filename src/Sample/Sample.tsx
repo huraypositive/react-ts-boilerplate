a231a69a073f7f7d4cbaed0400f4ca80a02d1cd3
yyounghoon <yhyoon@huray.net>
2022-05-20 17:54:44 +0900

    [추가] react-router-dom 설정

---
+++ src/Sample/Sample.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './Sample.styles';
+
+function Sample() {
+  console.log('sample');
+  return <Container>Sample</Container>;
+}
+export default Sample;
